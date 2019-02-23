8d0d8c2297eeeeef7bb389cc92d2394fef40f64c
Peter-Chou <zhou_00009@126.com>
2019-02-23 19:13:03 +0800

    update

--- evil-major-leader.el
+++ evil-major-leader.el
@@ -1,35 +1,34 @@
-;;; evil-major-leader.el --- makes major leader works in emacs -*- lexical-binding: t; -*-
+;;; evil-major-leader.el --- evil major leader mode    -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2019 by Peter Chou, all rights reserved.
+;; Copyright (C) 2019 by Peter Chou
 
-;; Author: Peter Chou <2747244153@qq.com>
-;; URL: https://github.com/Peter-Chou/evil-major-leader
-;; Version: 0.1
-;; Package-Version: 2019.0223
-;; Package-Requires: ((emacs "24.4") (evil 1.2.14) (evil-leader 0.4.3))
-;; Keywords: Evil, major leader
+;; Author:  Peter Chou <2747244153@qq.com>
+;; Homepage: https://github.com/Peter-Chou/evil-major-leader
+;; Version: 1.0.1
+;; Package-Requires: ((evil "0") (evil-leader "0.4.3"))
+;; Keywords: lisp evil
 
-;;; This file is NOT part of GNU Emacs
+;; This file is not part of GNU Emacs.
 
-;;; License
 ;;
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
-
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or
+;; (at your option) any later version.
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program; see the file COPYING.  If not, write to
 ;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
 ;; Floor, Boston, MA 02110-1301, USA.
-
+;;
 ;;; Commentary:
-;; evil-major-leader acts as a complement to evil-leader package.
+
+;; evil-major-leader acts as a complement to evil-leader package
 
 ;;; Code:
 
@@ -91,6 +90,4 @@
         (evil-major-leader-mode 1)))))
 
 (provide 'evil-major-leader)
-
 ;;; evil-major-leader.el ends here
-
